5eeb2bb2331158634a9c47bc340750f91d417e81
Nick <nikoloz.de@gmail.com>
2023-09-28 16:58:41 +0400

    Add project files.

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ValidParentheses
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sequenceOne = "(()())";
+            string sequenceTwo = "())()";
+            Console.WriteLine(IsProperly(sequenceOne));
+            Console.WriteLine(IsProperly(sequenceTwo));
+        }
+        public static bool IsProperly(string sequence)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char parentheses in sequence)
+            {
+                if (parentheses == '(')
+                {
+                    stack.Push('(');
+                }
+                else if (parentheses == ')')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
+
+    
